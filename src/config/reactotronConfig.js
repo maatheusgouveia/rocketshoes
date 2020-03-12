b8f58a601bfdd275fa787077b83379fa84095daa
Matheus Gouveia <matheus_guitarplayer@hotmail.com>
2020-03-11 23:47:24 -0300

    aplicação finalizada

--- src/config/reactotronConfig.js
+++ src/config/reactotronConfig.js
@@ -5,7 +5,7 @@ import reactotronSaga from "reactotron-redux-saga";
 if (process.env.NODE_ENV === "development") {
 	const tron = Reactotron.configure()
 		.use(reactotronRedux())
-		.use(reactotronSaga)
+		.use(reactotronSaga())
 		.connect();
 
 	tron.clear();
